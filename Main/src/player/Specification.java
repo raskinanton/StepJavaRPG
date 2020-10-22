040e50093518acbd1036affef018d1df68f1a548
SnipeZz-spec <goprosnipezz@yandex.ru>
2020-10-22 20:35:37 +0300

    Added players fields

--- Main/src/player/Specification.java
+++ Main/src/player/Specification.java
@@ -6,4 +6,33 @@ public class Specification {
     private int hpMax;
     private int coins;
     private int armor;
+
+    public Specification(String name, int hp, int hpMax, int coins, int armor) {
+        this.name = name;
+        this.hp = hp;
+        this.hpMax = hpMax;
+        this.coins = coins;
+        this.armor = armor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getHpMax() {
+        return hpMax;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
 }
+
