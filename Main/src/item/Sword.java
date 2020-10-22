a011d0c3031aac7410e7eb7689e0be278a646322
RomanManokhin <ghostrog@mail.ru>
2020-10-22 20:57:51 +0300

    new

--- Main/src/item/Sword.java
+++ Main/src/item/Sword.java
@@ -1,7 +1,13 @@
 package item;
 
-public class Sword {
+import player.AbstractPlayer;
+
+public class Sword implements item {
     private String name;
     private int damage;
 
+    @Override
+    public boolean apply(AbstractPlayer abstractPlayer) {
+        return false;
+    }
 }
--- Main/src/monsters/Monster.java
+++ Main/src/monsters/Monster.java
@@ -9,8 +9,6 @@ public class Monster implements IntMonster {
 
 
 
-
-
     @Override
     public int attack() {
         return 0;
