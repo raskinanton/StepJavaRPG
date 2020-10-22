2fb0a28876f316d6f38647bea7c7c085a78418e6
Взрослая академия <slsidorov@gmail.com>
2020-10-22 20:49:43 +0300

    Добавлен класс Game, реализующий игровой интерфейс. Добавлен класс Abs для обслуживания Game.java Добавлены строки в мейн для старта игры

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,43 @@
+import gameplay.Game;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scn = new Scanner(System.in);
+        int ex =-1;
         System.out.println("Hello comrades!!");
+        Game game = new Game();
+        while (true){
+            System.out.println("Введите 1 для начала игры или 0 для выхода");
+            String s = scn.nextLine();
+            if(checkInt(s)){
+                if(Integer.parseInt(s)==0){
+                    ex=0;
+                    break;
+                }
+                if(Integer.parseInt(s)==1){
+                    game.start();
+                    break;
+                }
+                else{
+                    System.out.println("Введено не соответствующее число");
+                }
+            }
+            else{
+                System.out.println("Введено не число. Повторите");
+            }
 
+        }
+
+    }
+    private static boolean checkInt(String s){
+        boolean bul = true;
+        try{
+            Integer.parseInt(s);
+        }catch (Exception e){
+            bul = false;
+        }
+        return bul;
     }
 }
--- Main/src/TestSerg.java
+++
@@ -1,5 +0,0 @@
-public class TestSerg {
-    public void hellow(){
-        System.out.println(" Я с вами. Сергей");
-    }
-}
