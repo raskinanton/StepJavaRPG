57d160114b3c010e6905f99bfecd3681f0219139
Sergey <SLSidorov@gmail.com>
2020-10-24 00:52:10 +0300

    update game. add finish by game over add get item by list<Item> from player

--- Main/src/gameplay/Game.java
+++ Main/src/gameplay/Game.java
@@ -11,8 +11,12 @@ public class Game extends Abs {
     private Scanner scn = new Scanner(System.in);
     private ArrayList<String> ar = new ArrayList<>();
     private ArrayList<String> label = new ArrayList(Arrays.asList( "maxhp", "coins", "armor", "attack"));
+    //вспомогательно поле на время отсутствия в игроке списка предметов
+    private ArrayList<String> itm = new ArrayList<>(Arrays.asList("монеты", "арбалет", "кольчуга", "бочка здоровья",
+            "мечь"));
     //плайер
     private Player player;
+    private boolean bulplay = true;
 
     public Game(){
         ar.add("Что бы сделать ход, введите - m");
@@ -21,53 +25,79 @@ public class Game extends Abs {
         ar.add("Что бы сделать поправить здоровье, введите - h");
         ar.add("Что бы использовать предмет, введите - t");
         ar.add("Что бы выйти из игры, введите - q");
+        //инициализируем игрока
         System.out.println("введите имя игрока");
         String name = scn.nextLine();
         ArrayList<Integer> ar = madePlayer();
-
         player = new Player(name, ar.get(0), ar.get(0), ar.get(1), ar.get(2), ar.get(3));
-        //player = new Player("игрок1", 20, 20, 0, 10);
-    }
+        }
 
     public void start(){
         System.out.println("Вы в игре");
         while (true){
             System.out.println("параметры игрока: ");
             //player.print();
+            //выводим статистические данны игрока собственным методом. Лучше сделать в игроке метод
             printPlayer(player);
             System.out.println("--------------");
             System.out.println("Выберите дальнейшую команду");
+            //выводим меню возможных действий игрока
+            // и предлагаем сделать выбор действия
             printMenu(ar);
             System.out.println("-------------");
             String s = scn.nextLine();
             if(s.equals("m") || s.equals("a") || s.equals("b") || s.equals("h") || s.equals("t") || s.equals("q")){
                 switch (s){
+                    //ход, после которго появится монстр. Делаем инициализацию монстра
                     case "m":
                         System.out.println("move");
-                        //
+                        //как вариант. По расчету данных игрока создаем монстра с соответствующими параметрами
+                        // Это образец
                         Monster monster = new Monster("name", 10, 10, 10);
                         System.out.println("-------------");
                         break;
                     case "a":
+                        // проводим атаку монстра. Рандомно кто первый бьет и каждый следующий удар, рандомно сила атаки
+                        //
                         System.out.println("atak");
+                        //проверка результата боя. Если игрок проиграл, придет false, if сработает, и после switch
+                        // сработает проверка на завершение игры по проигрышу
+//                        if(!monster.attack(player)){
+//                            bulplay = false;
+//                        }
                         System.out.println("-------------");
                         break;
                     case "b":
+                        // отступление, за деньги, которые надо отдать монстру
                         System.out.println("back");
                         System.out.println("-------------");
                         break;
                     case "h":
+                        //Лечение
                         System.out.println("add health");
                         System.out.println("-------------");
                         break;
                     case "t":
+                        //использование подобраных предметов в случае победы. Пересекается с монетами и лечением.
+                        // Надо уточнить
                         System.out.println(" use thing");
+                        //int numberItem = itemToDo(scn, player.getItems);
+                        //player.itemDo(number);
+                        //в методе по номеру item из списка, по его имени кейсами определяется что делать(увеличить
+                        // монет, защиту, и т.д.)
+                        itemToDo(scn,itm);
 
                         System.out.println("-------------");
                         break;
                 }
+                if(!bulplay){
+                    System.out.println("Выход из игры");
+                    System.out.println("Игрок погиб. Вы проиграли");
+                    break;
+                }
                 if(s.equals("q")){
                     System.out.println("Выход из игры");
+                    printPlayer(player);
                     break;
                 }
             }
--- Main/src/gameplay/menu/Abs.java
+++ Main/src/gameplay/menu/Abs.java
@@ -1,5 +1,8 @@
 package gameplay.menu;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Abs {
 
     public static boolean checkInt(String s) {
@@ -10,5 +13,36 @@ public class Abs {
             bul = false;
         }
         return bul;
+    }
+    public void itemToDo(Scanner scn, ArrayList<String> itm){
+        int it = -1;
+        while (true){
+            System.out.println("Выберите предмет,введя соответствующую цифру");
+            //player.getListItem().size
+            for(int i = 0;i<itm.size();i++){
+                System.out.println((i+1)+". "+itm.get(i));
+            }
+            System.out.println("Если передумали, введите q");
+            String itms = scn.nextLine();
+            if(itms.equals("q")){
+                break;
+            }
+            if(checkInt(itms)){
+                it = Integer.parseInt(itms);
+                if(it>0 && it<=itm.size()){
+                    break;
+                }
+                else{
+                    System.out.println("Введена команда не соответствующая меню");
+                }
+            }
+            else{
+                System.out.println("Введена команда не соответствующая меню");
+            }
+        }
+        if(it!=-1){
+            System.out.println("выбрали предмет - "+itm.get(it-1));
+            // todo
+        }
     }
 }
