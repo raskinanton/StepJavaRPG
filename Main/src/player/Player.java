5810d8881dfc468ec37b62ed97aa3db259c308cd
Sergey <SLSidorov@gmail.com>
2020-10-23 14:01:13 +0300

    В классе Player исправлены название полей в конструкторе на соответствие их названию в классе Specification

--- Main/src/player/Player.java
+++ Main/src/player/Player.java
@@ -3,8 +3,8 @@ package player;
 public class Player extends AbstractPlayer {
     private Specification spec;
 
-    public Player(String name, int a, int b, int c, int d){
-        this.spec = new Specification(name, a, b,c,d);
+    public Player(String name, int hp, int hpMax, int coins, int armor){
+        this.spec = new Specification(name, hp, hpMax,coins,armor);
     }
 
     public Specification getSpec() {
