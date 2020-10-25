f06701fbbbb4bef327d83478f3544f929105af09
aleksandr.dudnik <aadudnik@yandex.ru>
2020-10-25 22:48:59 +0300

    add in Monster class Map

--- Main/src/monsters/Monster.java
+++ Main/src/monsters/Monster.java
@@ -1,10 +1,14 @@
 package monsters;
 
+import item.*;
 import player.Player;
 import player.Specification;
 
+import java.util.*;
+
 public class Monster implements IntMonster {
     private Specification spec;
+//    private List<? super BossItem> itemsList = new ArrayList<>();
 
     public Monster(String name, int hp, int hpMax, int coins, int armor, int attack){
         this.spec = new Specification(name, hp, hpMax,coins,armor, attack);
@@ -20,6 +24,24 @@ public class Monster implements IntMonster {
         System.out.println("Количество монет - "+spec.getCoins());
         System.out.println("Защита - "+spec.getArmor());
         System.out.println("Атака - "+spec.getAttack());
+    }
+
+    public void initItem() {
+
+        Map<Integer, ? super BossItem> mapItem = new HashMap<>();
+        mapItem.put(1, new Coins("деньги", 10));
+        mapItem.put(2, new Crossbow("крутой арбалет", 20));
+        mapItem.put(3, new HealthBarrel("здоровье", 20));
+        mapItem.put(4, new PlateArmor("Пластинчатая броня", 30));
+        mapItem.put(5, new LeatherArmor("Кожанная броня", 30));
+        mapItem.put(6, new PlateArmor("Пластинчатая броня", 30));
+        mapItem.put(7, new Sword("меч", 30));
+
+        Random randomNumberCoins = new Random();
+        int numberCoins = randomNumberCoins.nextInt(7) + 1;
+
+//        itemsList.add(mapItem.get(1));
+
     }
 
     @Override
