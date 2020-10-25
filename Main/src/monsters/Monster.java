dad757158785688975419234c23b08f55c92ea00
Ghost-Rog <ghostrog@mail.ru>
2020-10-25 14:28:20 +0300

    add specification to Monster

--- Main/src/monsters/Monster.java
+++ Main/src/monsters/Monster.java
@@ -3,25 +3,20 @@ package monsters;
 import player.Player;
 import player.Specification;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Monster implements IntMonster {
+    private Specification spec;
 
-    private String monsterName;
-    private int damage;
-    private int heals;
-    private int payCoins;
-
-    public Monster(String monsterName, int damage, int heals, int payCoins) {
-        this.monsterName = monsterName;
-        this.damage = damage;
-        this.heals = heals;
-        this.payCoins = payCoins;
+    public Monster(String name, int hp, int hpMax, int coins, int armor, int attack){
+        this.spec = new Specification(name, hp, hpMax,coins,armor, attack);
     }
 
+    public Specification getSpec() {
+        return spec;
+    }
+
+
     @Override
-    public boolean attack(Player player) {
+    public boolean attack(Player player, Monster monster) {
 
 //        Specification specPlayer = player.getSpec();
 //
@@ -31,9 +26,10 @@ public class Monster implements IntMonster {
     }
 
     @Override
-    public boolean pass(Player player) {
+    public boolean pass(Player player, Monster monster) {
         Specification specPlayer = player.getSpec();
-        return specPlayer.getCoins() > payCoins;
+        Specification specMonster = monster.getSpec();
+        return specPlayer.getCoins() < specMonster.getCoins();
 
     }
 }
