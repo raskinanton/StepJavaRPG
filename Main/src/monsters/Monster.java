adb677493120527b37b7298a9549c025acb5fc08
Ghost-Rog <ghostrog@mail.ru>
2020-10-25 23:14:25 +0300

    correcting Monster

--- Main/src/monsters/Monster.java
+++ Main/src/monsters/Monster.java
@@ -10,6 +10,7 @@ public class Monster implements IntMonster {
     private Specification spec;
     private List<BossItem> itemsList = new ArrayList<>();
 
+
     public Monster(String name, int hp, int hpMax, int coins, int armor, int attack) {
         this.spec = new Specification(name, hp, hpMax, coins, armor, attack);
     }
@@ -33,18 +34,18 @@ public class Monster implements IntMonster {
     public void initItem() {
 
         Map<Integer, BossItem> mapItem = new HashMap<>();
-        mapItem.put(1, new Coins("деньги", 10));
-        mapItem.put(2, new Crossbow("крутой арбалет", 20));
-        mapItem.put(3, new HealthBarrel("здоровье", 20));
-        mapItem.put(4, new PlateArmor("Пластинчатая броня", 30));
-        mapItem.put(5, new LeatherArmor("Кожанная броня", 30));
+        mapItem.put(1, new Coins("Деньги", 10));
+        mapItem.put(2, new Crossbow("Крутой арбалет", 20));
+        mapItem.put(3, new HealthBarrel("Бутылка со здоровьем", 20));
+        mapItem.put(4, new HauberkArmor("Броня Хауберка", 50));
+        mapItem.put(5, new LeatherArmor("Кожанная броня", 15));
         mapItem.put(6, new PlateArmor("Пластинчатая броня", 30));
-        mapItem.put(7, new Sword("меч", 30));
+        mapItem.put(7, new Sword("Меч", 30));
 
         Random randomNumberCoins = new Random();
         int numberCoins = randomNumberCoins.nextInt(7) + 1;
 
-        itemsList.add(mapItem.get(1));
+        itemsList.add(mapItem.get(numberCoins));
 
     }
 
