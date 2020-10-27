4c64abcdde46736171172ea0bccec75bf57f2180
RomanManokhin <ghostrog@mail.ru>
2020-10-27 20:00:02 +0300

    Junit test MonsterTest

---
+++ Main/test/monsters/MonsterTest.java
@@ -0,0 +1,22 @@
+package monsters;
+
+
+import org.junit.jupiter.api.Test;
+import player.Player;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MonsterTest {
+    @Test
+    public void passTest(){
+        Monster monster = new Monster("Monster", 100, 100, 60, 10, 5);
+
+        Player player = new Player("Sasha", 100, 100, 50, 10,5);
+        Player player1 = new Player("Sasha", 100, 100, 50, 10,5);
+
+        var res = monster.pass(player);
+        var res1 = monster.pass(player1);
+        assertTrue(res);
+        assertFalse(res1);
+//        return player.getSpec().getCoins() < spec.getCoins();
+    }
+}
