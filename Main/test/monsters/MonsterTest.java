b8980360a7e495066494de31d7db0f20ff29a6bd
RomanManokhin <ghostrog@mail.ru>
2020-10-27 20:18:34 +0300

    Junit

--- Main/test/monsters/MonsterTest.java
+++ Main/test/monsters/MonsterTest.java
@@ -11,12 +11,23 @@ public class MonsterTest {
         Monster monster = new Monster("Monster", 100, 100, 60, 10, 5);
 
         Player player = new Player("Sasha", 100, 100, 50, 10,5);
-//        Player player1 = new Player("Sasha", 100, 100, 50, 10,5);
 
         var res = monster.pass(player);
 
         assertTrue(res);
 
-//        return player.getSpec().getCoins() < spec.getCoins();
     }
+
+    @Test
+    public void passTest1(){
+        Monster monster = new Monster("Monster", 100, 100, 60, 10, 5);
+
+        Player player1 = new Player("Sasha", 100, 100, 50, 10,5);
+
+        var res = monster.pass(player1);
+
+        assertTrue(res);
+
+    }
+
 }
